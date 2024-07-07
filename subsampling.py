4c8b433ab8230ffe81f4e65097c9cff3c7bfadc8
Michail Mitsakis <73486764+michailmitsakis@users.noreply.github.com>
2024-07-07 13:46:41 +0300

    Delete subsampling.py

--- subsampling.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def random_subsample(df, num_samples):
-    np.random.seed(42)
-    indices = np.random.choice(df.index, num_samples, replace=False)
-    subsampled_df = df.loc[indices]
-    return subsampled_df 
